d1b3935c4f0803b0cb781baafd5afab59384cd11
Anders Østby <anders.ostby@nav.no>
2022-07-27 10:10:21 +0200

    Definerer topics og oppretter deploy matrix

--- app/src/main/kotlin/no/nav/aap/app/kafka/Topics.kt
+++ app/src/main/kotlin/no/nav/aap/app/kafka/Topics.kt
@@ -6,8 +6,8 @@ import no.nav.aap.kafka.streams.Topic
 
 object Topics {
     //TODO: Finn riktig navn på topic
-    val spleis = Topic("aap.spleis.v1", JsonSerde.jackson<SykepengedagerKafkaDto>())
+    val spleis = Topic("aap.sykepengedager.spleis.v1", JsonSerde.jackson<SykepengedagerKafkaDto>())
     //TODO: Finn riktig navn på topic
-    val infotrygd = Topic("aap.infotrygd.v1", JsonSerde.jackson<SykepengedagerKafkaDto>())
+    val infotrygd = Topic("aap.sykepengedager.infotrygd.v1", JsonSerde.jackson<SykepengedagerKafkaDto>())
     val sykepengedager = Topic("aap.sykepengedager.v1", JsonSerde.jackson<SykepengedagerKafkaDto>())
 }
