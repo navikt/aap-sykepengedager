db9812429c6a7af6787fa416e5a270b59ca764f7
Anders Østby <anders.ostby@nav.no>
2022-10-17 13:04:24 +0200

    Bumper og bruker StateStoreMigrator fra libs

--- app/src/main/kotlin/no/nav/aap/app/App.kt
+++ app/src/main/kotlin/no/nav/aap/app/App.kt
@@ -13,7 +13,6 @@ import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import no.nav.aap.app.kafka.Tables
 import no.nav.aap.app.kafka.Topics
-import no.nav.aap.app.kafka.migrateStateStore
 import no.nav.aap.app.stream.infotrygdStream
 import no.nav.aap.app.stream.reproduce
 import no.nav.aap.app.stream.spleisStream
@@ -23,6 +22,7 @@ import no.nav.aap.kafka.streams.KStreamsConfig
 import no.nav.aap.kafka.streams.KafkaStreams
 import no.nav.aap.kafka.streams.extension.consume
 import no.nav.aap.kafka.streams.extension.produce
+import no.nav.aap.kafka.streams.store.migrateStateStore
 import no.nav.aap.kafka.streams.store.scheduleMetrics
 import no.nav.aap.kafka.vanilla.KafkaConfig
 import no.nav.aap.ktor.config.loadConfig
